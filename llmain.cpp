c92451dba06a4a46c5f2bb6be2089947f8358a96
AnoushSim <39119965+AnoushSim@users.noreply.github.com>
2018-07-04 16:32:35 +0400

    Update llmain.cpp

--- llmain.cpp
+++ llmain.cpp
@@ -6,10 +6,14 @@ int main() {
         l_list.insert(1);
         l_list.insert(2);
         l_list.insert(3);
+        l_list.showList();
+        std::cout << "\n";
       	l_list.insert_at(2, 4);
+      	l_list.showList();
+        std::cout << "\n";
       	int position = l_list.find(4);
-	std::cout << position <<std::endl;
+      	std::cout << position <<std::endl;
       	l_list.remove_(3);
-	l_list.showList();
-      	return 0;
+        l_list.showList();
+	return 0;
 }
